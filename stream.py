54b89514bdb3c0ae09a0e85e0e2e7df46c78c239
jbiju94 <jbiju94@gmail.com>
2018-01-11 17:30:27 +0530

    Implementations: --- * Sentiment Analysis * Json Response

--- stream.py
+++ stream.py
@@ -2,6 +2,8 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import global_const as const
+import json
+from textblob import TextBlob
 
 # Variables that contains the user credentials to access Twitter API
 access_token = const.access_token
@@ -14,8 +16,23 @@ consumer_secret = const.consumer_secret
 class Listener(StreamListener):
 
     def on_data(self, data):
-        print data
-        return True
+        tweet = json.loads(data)
+        if tweet['retweeted']:
+            return
+
+        blob = TextBlob(tweet['text'])
+        sent = blob.sentiment
+
+        polarity = sent.polarity
+        subjectivity = sent.subjectivity
+
+        positive_sentiment = True
+        if polarity < 0:
+            positive_sentiment = False
+
+        tweet['positive_sentiment'] = positive_sentiment
+        print {"text": tweet['text'], "userId": tweet['user']['name'], "userName": tweet['user']['screen_name'],
+               "positiveSentiment": tweet['positive_sentiment']}
 
     def on_error(self, status):
         print status
---
+++ tweet.py
@@ -0,0 +1,10 @@
+
+class Tweet:
+    def __init__(self,user_id, user_name, tweet_text, tweet_location, retweet_count, tweet_coordinates):
+        self.user_id = user_id,
+        self.user_name = user_name,
+        self.text = tweet_text,
+        self.location = tweet_location,
+        self.retweet_count = retweet_count,
+        self.coordinates = tweet_coordinates
+
